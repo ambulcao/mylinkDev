aba7a75203f325697863fbe2cf7dd18ae7ccc1be
Alexandre Marques Bulcao <ambulcao@gmail.com>
2022-02-04 21:12:44 +0000

    Creating layout app

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,25 @@
+import { FiLink } from 'react-icons/fi'
+import './home.css'
+
 export default function Home(){
     return(
-      <div>
-        <h2>Página Home!</h2>
+      <div className="container-home">
+        <div className="logo">
+            <img src="../../assets/pitu2.jpeg" alt="Logo Pitu v2" />
+            <h1>PituLink</h1>
+            <span>Cole seu link para encurtar 👇</span>
+        </div>
+
+        <div className="area-input">
+            <div>
+             <FiLink size={24} color="#FFF"/>
+             <input 
+                placeholder='Cole seu link aqui...'
+             />
+            </div>
+
+            <button>Gerar Link</button>
+        </div>
       </div>
     )
   }

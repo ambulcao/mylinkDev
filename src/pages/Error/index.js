45c794d64388559105da61ef62dff016e624a570
Alexandre Marques Bulcao <ambulcao@gmail.com>
2022-02-08 22:09:08 +0000

    Creating page notFound

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -1,10 +1,16 @@
 
 import './error.css'
+import {Link} from 'react-router-dom'
+import ImgNotFound from '../../assets/notfound.png'
 
 export default function Error() {
     return (
-        <div>
-            <h1>TELA 404</h1>
+        <div className='container-error'>
+            <img className="notfound" src={ImgNotFound} alt="Pagina nao encontrada" />
+            <h1>Página não encontrada</h1>
+            <Link to="/">
+                Voltar para a home
+            </Link>
         </div>
     )
 }

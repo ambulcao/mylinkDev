2dc3abb5a6f2ce67764ceba97db05fa73ac3c5a7
Alexandre Marques Bulcao <ambulcao@gmail.com>
2022-02-16 21:40:15 +0000

    Creating API Bitly

---
+++ src/services/api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export const key = "f43f9093943c56940f5a20b01e25c3cc3a7f7469";
+
+const api = axios.create({
+    baseURL: 'https://api-ssl.bitly.com/v4/shorten',
+    headers:{
+        'Authorization': `Bearer ${key}`
+    }
+})
+
+export default api;

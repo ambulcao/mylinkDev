7decd1db6a63be598147f98796994ec113d47cd6
ambulcao <ambulcao@gmail.com>
2022-02-19 19:32:28 +0000

    copyLink URL shortened

--- src/components/LinkItem/index.js
+++ src/components/LinkItem/index.js
@@ -3,6 +3,12 @@ import './link-item.css'
 import { FiX, FiClipboard } from 'react-icons/fi'
 
 export default function LinkItem({ closeModal, content }) {
+
+    async function copyLink(){
+        await navigator.clipboard.writeText(content.link)
+        alert("URL Copiada com sucesso!");
+    }
+
     return (
         <div className='modal-container'>
             <div className='modal-header'>
@@ -16,7 +22,7 @@ export default function LinkItem({ closeModal, content }) {
                 {content.long_url}
             </span>
 
-            <button className='modal-link'>
+            <button className='modal-link' onClick={copyLink}>
                 {content.link}
                 <FiClipboard size={20} color="#FFF" />
             </button>

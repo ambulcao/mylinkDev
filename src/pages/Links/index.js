3dc741623c00676eec87089e0e7e1e175d1ba0d8
ambulcao <ambulcao@gmail.com>
2022-02-21 21:16:58 +0000

    .Map from my links

--- src/pages/Links/index.js
+++ src/pages/Links/index.js
@@ -1,8 +1,30 @@
+import { useState, useEffect } from 'react'
 import './links.css'
 import { FiArrowLeft, FiLink, FiTrash } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
 
+import { getLinksSave } from '../../services/storeLinks'
+
 export default function Links() {
+
+  const [myLinks, setMyLinks] = useState([]);
+
+  const [data, setData] = useState({});
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    async function getLinks(){
+      const result = await getLinksSave('@encurtaLink')
+
+      if(result.length === 0){
+        //lista está vazia...
+        console.log('Lista vazia')
+      }
+      setMyLinks(result);
+    }
+    getLinks();
+  }, [])
+
   return (
     <div className='links-container'>
 
@@ -13,25 +35,17 @@ export default function Links() {
         <h1>Meus Links</h1>
       </div>
 
-      <div className='link-item'>
+    { myLinks.map( link => (
+        <div key={link.id} className='links-item'>
         <button className='link'>
           <FiLink size={18} color="#FFF" />
-          https://pitulink.com
+          {link.long_url}
         </button>
         <button className='link-delete'>
           <FiTrash size={24} color="#FF5454" />
         </button>
       </div>
-
-      <div className='link-item'>
-        <button className='link'>
-          <FiLink size={18} color="#FFF" />
-          https://pitulink.com
-        </button>
-        <button className='link-delete'>
-          <FiTrash size={24} color="#FF5454" />
-        </button>
-      </div>
+    ))}
 
     </div>
   )
